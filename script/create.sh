67e0a6cb71cb7e9c1590cb342a417a58b3b51aa9
hooks_28 <1r56955415918@qq.com>
2023-04-16 09:50:59 +0800

    feat(script): update create.sh

--- script/create.sh
+++ script/create.sh
@@ -1,9 +1,9 @@
 ###
 # @Author: 邱狮杰
 # @Date: 2023-04-16 09:03:29
-# @LastEditTime: 2023-04-16 09:03:30
+ # @LastEditTime: 2023-04-16 09:50:36
 # @Description:
-# @FilePath: /baseRepo/script/create.sh
+ # @FilePath: /baseRepo/script/create.sh
 ###
 
 mkdir etc
@@ -15,3 +15,11 @@ touch src/index.ts
 pnpm init
 
 tsc --init
+
+npm pkg set scripts.build:cjs='tsc --module commonjs --rootDir ./src --outDir ./lib --experimentalDecorators true'
+
+npm pkg set scripts.build:es='tsc --module es6 --rootDir ./src --outDir ./dist --experimentalDecorators true'
+
+npm pbk set scripts.build="pnpm build:cjs && pnpm build:es"
+
+npm pkg set scripts.build:md="pnpm build && api-extractor run --local --verbose && sh mv.sh"
