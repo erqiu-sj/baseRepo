5d55e370f631e8ce4ec6c44eb5389ade74e055b3
hooks_28 <1r56955415918@qq.com>
2023-04-16 09:06:03 +0800

    Keep calm and commit

---
+++ script/create.sh
@@ -0,0 +1,17 @@
+###
+# @Author: 邱狮杰
+# @Date: 2023-04-16 09:03:29
+# @LastEditTime: 2023-04-16 09:03:30
+# @Description:
+# @FilePath: /baseRepo/script/create.sh
+###
+
+mkdir etc
+
+mkdir src
+
+touch src/index.ts
+
+pnpm init
+
+tsc --init
---
+++ script/setup.sh
@@ -0,0 +1,7 @@
+###
+ # @Author: 邱狮杰
+ # @Date: 2023-04-16 09:03:21
+ # @LastEditTime: 2023-04-16 09:03:23
+ # @Description: 
+ # @FilePath: /baseRepo/script/setup.sh
+### 
